8fa1d85fb1f1ef717cf4e54cc00c7571f6ffbb1c
Muhammad <areebqureshi1122@gmail.com>
2024-07-18 21:00:15 +0500

    Added new feature in TextUtils

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,11 +6,16 @@ export default function TextForm(props) {
     let newText = text.toUpperCase();
     setText(newText);
   };
+  const handleDownClick = () => {
+    
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
   const handleOnChange = (event) => {
     // console.log("On Change");
     setText(event.target.value);
   };
-  const [text, setText] = useState("Enter text here");
+  const [text, setText] = useState("");
   // setText('Write something here');
   return (
     <div>
@@ -24,9 +29,17 @@ export default function TextForm(props) {
           rows="8"
         ></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>
+      <button className="btn btn-primary mx-1" onClick={handleUpClick}>
         Convert to Uppercase
       </button>
+      <button className="btn btn-primary mx-1" onClick={handleDownClick}>Convert to LowerCase</button>
+      <div className="container my-4" >
+        <h1>Your Text Summary</h1>
+    <p> {text.split(" ").length} words , {text.length} character</p>
+    <p>{ 0.008 * text.split(" ").length} Minutes Read</p>
+    <h2>Preview</h2>
+    <p> {text}</p>
+      </div>
     </div>
   );
 }
